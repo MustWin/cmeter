1d5dd682af73542af246171d8e36361ca27a8b24
Daniel Krainas <dkrainas87@gmail.com>
2016-09-16 15:43:06 -0400

    add basic reporting package

---
+++ reporting/reporting.go
@@ -0,0 +1,20 @@
+package reporting
+
+import (
+	"github.com/MustWin/cmeter/configuration"
+	"github.com/MustWin/cmeter/context"
+)
+
+type Receipt string
+
+type Event struct {
+	MeterID    string      `json:"meter_id"`
+	Type       string      `json:"event_type"`
+	ServiceKey string      `json:"service_key"`
+	Timestamp  int64       `json:"timestamp"`
+	Data       interface{} `json:"data"`
+}
+
+type Driver interface {
+	Report(ctx context.Context, e *Event) (string, error)
+}
